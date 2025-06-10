b50f41f516a9564b342bbc1dad88629e6d604d02
equation314 <equation618@gmail.com>
2025-06-10 17:36:25 +0000

    Deploying to gh-pages from @ arceos-org/lazyinit@6ffb6e838d7cf3b17917cfd7dd188ec5fcd341b5 🚀

---
+++ trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["lazyinit",[["impl&lt;T&gt; UnsafeUnpin for <a class=\"struct\" href=\"lazyinit/struct.LazyInit.html\" title=\"struct lazyinit::LazyInit\">LazyInit</a>&lt;T&gt;<div class=\"where\">where\n    T: UnsafeUnpin,</div>",1,["lazyinit::LazyInit"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[243]}
---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["lazyinit",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"lazyinit/struct.LazyInit.html\" title=\"struct lazyinit::LazyInit\">LazyInit</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[292]}
